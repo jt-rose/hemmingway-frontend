182f11428d68f0db1e59023620fb94a4fee66a81
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-24 17:35:15 -0400

    update backend url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-import Gallery from '../components/Gallery'
+import Gallery from "../components/Gallery";
+import axios from "axios";
+import { useEffect } from "react";
 
-const Index = () => <Gallery />
+const Index = () => {
+  useEffect(() => {
+    axios
+      .post("https://hemmingway.herokuapp.com/graphql", {
+        query: `{
+            me {
+              name
+            }
+          }`,
+      })
+      .then((response) => console.log(response));
+  }, []);
+  return <Gallery />;
+};
 
-export default Index
+export default Index;
