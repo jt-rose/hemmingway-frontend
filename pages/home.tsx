ac819c06d24fcd0e28ba9cb10d6f63eb98f10e3e
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-28 11:38:31 -0400

    testing codegen

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,10 @@
 import { Layout } from "components/Layout";
 import React from "react";
-import { useQueryClient, useQuery } from "react-query";
 import { useMeQuery } from "src/generated/graphql-hooks";
 import { client } from "utils/client";
 
 const Home = () => {
-  const { data, error, isLoading } = useMeQuery(client);
+  const { data } = useMeQuery(client);
 
   console.log("data: ", data);
 
