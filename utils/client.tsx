0b0fadd9c4be7d28d0d47ed54606aaa249506f19
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-29 00:08:46 -0400

    set up function to init client

--- utils/client.tsx
+++ utils/client.tsx
@@ -5,4 +5,10 @@ const endpoint =
     ? "https://hemmingway.herokuapp.com/graphql"
     : "http://localhost:8000/graphql";
 
-export const client = new GraphQLClient(endpoint);
+export const initClient = (token: string) =>
+  new GraphQLClient(endpoint, {
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
