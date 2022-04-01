a45cfcfe0f904a176fdf6b9b2a2898c669284cb6
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-01 00:57:34 -0400

    adjust color on radio buttons

--- components/forms/Radio.tsx
+++ components/forms/Radio.tsx
@@ -9,7 +9,7 @@ export const Radio = (props: { radioOptions: string[]; name: string }) => {
             type="radio"
             name={props.name}
             value={ro}
-            className="w-4 h-4 border-gray-300 appearance-none checked:bg-teal-500 rounded-xl border-2 checked:border-teal-600 dark:accent-teal-700 dark:focus:bg-teal-600 dark:bg-gray-700 dark:border-gray-600"
+            className="w-4 h-4 border-gray-300 appearance-none checked:bg-teal-400 rounded-xl border-2 checked:border-teal-500 dark:accent-teal-700 dark:focus:bg-teal-600 dark:bg-gray-700 dark:border-gray-600"
             aria-labelledby={ro + "-radio-" + index}
             aria-describedby={ro + "-radio-" + index}
           />
