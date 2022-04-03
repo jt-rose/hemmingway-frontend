6cca0be385df26e20f3e19372d981996f8277e2f
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-02 22:25:28 -0400

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,92 +6,37 @@ import {
   CalendarIcon,
 } from "@heroicons/react/outline";
 
-const links = [
-  {
-    href: "/home",
-    linkTitle: "Home",
-  },
-  {
-    href: "/stats",
-    linkTitle: "Stats",
-  },
-  {
-    href: "/settings",
-    linkTitle: "Settings",
-  },
-];
-
 export const Navbar = () => {
   return (
     <nav className="flex justify-between bg-teal-400 p-2 px-4 rounded-lg m-2">
-      <p className="text-slate-50 text-left pl-8 pt-2">Hemmingway</p>
-      <ul className="text-slate-50 grow text-center flex justify-around">
-        {links.map((link) => (
-          <li
-            className="hover:bg-teal-500 rounded-lg p-2"
-            key={"link-" + link.linkTitle}
-          >
-            <Link href={link.href}>
-              <a className="text-slate-50 font-bold">{link.linkTitle}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <CalendarIcon
-        className="text-slate-50 p-2 ht-10 w-10 hover:bg-teal-500 rounded-full"
-        stroke="rgb(248 250 252)"
-      />
-      <ChartBarIcon
-        className="text-slate-50 p-2 ht-10 w-10 hover:bg-teal-500 rounded-full"
-        stroke="rgb(248 250 252)"
-      />
-      {/* <UserIcon className="text-slate-50 grow text-right pr-8 pt-2 " /> */}
-      <UserCircleIcon
-        className="text-slate-50 p-2 ht-10 w-10 hover:bg-teal-500 rounded-full"
-        stroke="rgb(248 250 252)"
-      />
+      <Link href="/home">
+        <div className="flex hover:bg-teal-500 rounded-full hover:cursor-pointer">
+          <CalendarIcon
+            className="text-slate-50 p-2 ht-10 w-10 "
+            stroke="rgb(248 250 252)"
+          />{" "}
+          <p className="text-slate-50 font-bold  pr-2 pt-2">Daily</p>
+        </div>
+      </Link>
+      <Link href="/stats">
+        <div className="flex hover:bg-teal-500 rounded-full hover:cursor-pointer">
+          <ChartBarIcon
+            className="text-slate-50 p-2 ht-10 w-10 hover:bg-teal-500 rounded-full"
+            stroke="rgb(248 250 252)"
+          />{" "}
+          <p className="text-slate-50 font-bold  pr-2 pt-2">Stats</p>
+        </div>
+      </Link>
+
+      <Link href="/settings">
+        <div className="flex hover:bg-teal-500 rounded-full hover:cursor-pointer">
+          <UserCircleIcon
+            className="text-slate-50 p-2 ht-10 w-10 hover:bg-teal-500 rounded-full"
+            stroke="rgb(248 250 252)"
+          />{" "}
+          <p className="text-slate-50 font-bold  pr-2 pt-2">Account</p>
+        </div>
+      </Link>
     </nav>
-    //<Example />
-    //   <div>
-    //     <Flex bg="teal" w="100%" p={4} color="white">
-    //       <Text color="whitesmoke" fontFamily="cursive" fontSize="2xl">
-    //         Hemmingway
-    //       </Text>
-    //       <Spacer />
-    //       <IconButton
-    //         aria-label="user-settings"
-    //         color="white"
-    //         bg="teal"
-    //         icon={<UserIcon />}
-    //         variant="outline"
-    //       />
-    //       <Spacer />
-    //       <Menu>
-    //         <MenuButton
-    //           as={IconButton}
-    //           aria-label="Options"
-    //           icon={<HamburgerIcon />}
-    //           variant="outline"
-    //         />
-    //         <MenuList>
-    //           <MenuItem icon={<AddIcon />} command="⌘T">
-    //             New Tab
-    //           </MenuItem>
-    //           <MenuItem icon={<ExternalLinkIcon />} command="⌘N">
-    //             New Window
-    //           </MenuItem>
-    //           <MenuItem icon={<RepeatIcon />} command="⌘⇧N">
-    //             Open Closed Tab
-    //           </MenuItem>
-    //           <MenuItem icon={<EditIcon />} command="⌘O">
-    //             Open File...
-    //           </MenuItem>
-    //         </MenuList>
-    //       </Menu>
-    //     </Flex>
-    // <div>
-    //   <Example />
-    //   <Tail />
-    // </div>
   );
 };
