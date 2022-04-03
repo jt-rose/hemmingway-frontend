6d13c24fb18c9cdc6bf02b0ec2ea7ac1b23f5054
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-03 06:20:30 -0400

    update styling of meal card

--- components/exercise/ExerciseCard.tsx
+++ components/exercise/ExerciseCard.tsx
@@ -25,7 +25,7 @@ export const ExerciseCard = (props: {
   return (
     <div className="flex flex-col sm:flex-row items-center bg-white rounded-lg border shadow-md max-w-xl md:max-w-2xl lg:max-w-3xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-4">
       <img
-        className="sm:block object-cover h-24 sm:h-full w-full rounded-t-lg sm:w-48 sm:rounded-none sm:rounded-l-lg"
+        className="object-cover h-24 sm:h-full w-full rounded-t-lg sm:w-48 sm:rounded-none sm:rounded-l-lg"
         src="/exercise-1.jpg"
         alt=""
       />
