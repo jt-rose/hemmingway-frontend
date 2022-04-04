c963db6e9cd58f8817660e739a7b2aec72c653ec
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-04 14:46:12 -0400

    remov duplicate font size

--- components/forms/UpdateMoodModal.tsx
+++ components/forms/UpdateMoodModal.tsx
@@ -71,7 +71,7 @@ export function UpdateMoodModal(props: PropTypesWithMood) {
                 {/* <div className="mt-4 flex justify-center">
                   <button
                     type="button"
-                    className="text-white font-bold bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white font-bold bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     onClick={closeModal}
                   >
                     Cancel
