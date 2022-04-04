106909468e54b9bd2617d8ae6a481d0366d1bdb5
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-04 10:22:48 -0400

    add footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 import { Dispatch, ReactNode, SetStateAction } from "react";
 import { Navbar } from "./Navbar";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
 
 // add Head
 
@@ -13,6 +15,25 @@ export const Layout = (props: {
     <main>
       <Navbar gqlClient={props.gqlClient} setToken={props.setToken} />
       {props.children}
+      <footer className=" bg-teal-500  mt-8 border-t-2 border-teal-600 py-2">
+        <p className="text-center text-slate-50">Designed by Jeff Rose, 2022</p>
+        <div className="flex justify-center mt-1">
+          <a
+            href="https://www.linkedin.com/in/jeffreytrose"
+            target="_blank"
+            className="text-xl mx-4 hover:cursor-pointer hover:bg-teal-600 p-2 rounded-md"
+          >
+            <AiFillLinkedin fill="rgb(248 250 252)" stroke="rgb(20 184 166)" />
+          </a>
+          <a
+            href="https://github.com/jt-rose"
+            target="_blank"
+            className="text-xl mx-4 hover:cursor-pointer hover:bg-teal-600 p-2 rounded-md"
+          >
+            <AiFillGithub fill="rgb(248 250 252)" stroke="rgb(20 184 166)" />
+          </a>
+        </div>
+      </footer>
     </main>
   );
 };
