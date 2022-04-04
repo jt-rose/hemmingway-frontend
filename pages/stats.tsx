1882a8782e3d89be6df0c4f2334196de838f5b1a
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-03 23:55:55 -0400

    update styles of stats dates input

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -30,6 +30,7 @@ import { VPieFull } from "components/charts/VPieFull";
 import { Pie } from "components/charts/Pie";
 import { UseQueryResult } from "react-query";
 import { associateCaloriesByDates } from "utils/getDailyCalories";
+import { useForm } from "react-hook-form";
 
 const countMoodType = (
   moodData: UseQueryResult<MoodsBetweenDatesQuery, unknown>,
@@ -71,8 +72,11 @@ const countSleepQuality = (
     : 1;
 
 const Stats = (props: PropTypesWithRefresh) => {
-  const [from, setFrom] = useState("1900-01-01");
-  const [to, setTo] = useState(dayjs().format("YYYY-MM-DD"));
+  const today = dayjs().format("YYYY-MM-DD");
+  const [from, setFrom] = useState(
+    dayjs().subtract(1, "month").format("YYYY-MM-DD")
+  );
+  const [to, setTo] = useState(today);
 
   const weightHistory = useWeightsBetweenDatesQuery(props.gqlClient, {
     dateRange: { from, to },
@@ -101,7 +105,6 @@ const Stats = (props: PropTypesWithRefresh) => {
   const sleepHabits = useSleepHabitsBetweenDatesQuery(props.gqlClient, {
     dateRange: { from, to },
   });
-
   const me = useMeQuery(props.gqlClient);
 
   type AssocResults = {
@@ -135,20 +138,56 @@ const Stats = (props: PropTypesWithRefresh) => {
   return (
     <Layout gqlClient={props.gqlClient} setToken={props.setToken}>
       <h1 className="text-2xl pl-8">Stats</h1>
-      <label htmlFor="from-date">From: </label>
+      {/* <label htmlFor="from-date">From: </label>
       <input
         id="from-date"
         type="date"
         value={from}
         onChange={(e) => setFrom(e.target.value)}
-      />
-      <label htmlFor="to-date">To: </label>
+      /> */}
+      <div className="mx-6 max-w-sm flex flex-col justify-items-center w-full">
+        <div className="mb-6">
+          <label
+            htmlFor="from-date"
+            className="block font-bold text-center mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            From
+          </label>
+          <input
+            type="date"
+            id="from-date"
+            className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="..."
+            required
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+          />
+        </div>
+        {/* <label htmlFor="to-date">To: </label>
       <input
         id="to-date"
         type="date"
         value={to}
         onChange={(e) => setTo(e.target.value)}
-      />
+      /> */}
+        <div className="mb-6">
+          <label
+            htmlFor="to-date"
+            className="block font-bold text-center mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            To
+          </label>
+          <input
+            type="date"
+            id="to-date"
+            className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="..."
+            required
+            value={to}
+            onChange={(e) => setTo(e.target.value)}
+          />
+        </div>
+      </div>
       Weights: {weightHistory.data?.weightBetweenDates.length}
       Weight Goals: {weightGoals.data?.weightGoalsBetweenDates.length}
       Distance Goals: {distanceGoals.data?.distanceGoalsBetweenDates.length}
