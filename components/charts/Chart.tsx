990ab062d2f041825eb85f339844fe46a796c3f1
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-04 09:46:00 -0400

    remove unused code

--- components/charts/Chart.tsx
+++ components/charts/Chart.tsx
@@ -1,36 +1,15 @@
-import {
-  VictoryChart,
-  VictoryTheme,
-  VictoryLine,
-  VictoryLabel,
-  VictoryScatter,
-  VictoryAxis,
-  VictoryZoomContainer,
-} from "victory";
+import { VictoryChart, VictoryTheme, VictoryLine, VictoryAxis } from "victory";
 
 export const Chart = (props: { data: { x: string; y: number }[] }) => (
-  <VictoryChart
-    theme={VictoryTheme.material}
-    //containerComponent={<VictoryZoomContainer />}
-  >
+  <VictoryChart theme={VictoryTheme.material}>
     <VictoryLine
-      //interpolation="natural"
-      //labels={({ datum }) => datum.y}
-      //labelComponent={<VictoryLabel renderInPortal dy={-20} />}
       style={{
         data: { stroke: "#c43a31", strokeWidth: 5 },
         parent: { border: "1px solid #ccc" },
       }}
       data={props.data}
-
-      // domain={{ y: [500, -600] }}
-      //categories={["hi", "bye"]}
     />
     <VictoryAxis dependentAxis fixLabelOverlap />
-    <VictoryAxis
-      fixLabelOverlap
-      //tickValues={[1, 2, 3, 4, 5]}
-      //style={{ tickLabels: { angle: 45 } }}
-    />
+    <VictoryAxis fixLabelOverlap />
   </VictoryChart>
 );
