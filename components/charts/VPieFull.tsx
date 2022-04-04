425e8354ade0e224b8f50f78c781a867a095cd8d
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-04 11:05:42 -0400

    update stats page styling

--- components/charts/VPieFull.tsx
+++ components/charts/VPieFull.tsx
@@ -9,6 +9,7 @@ export const VPieFull = (props: { data: { x: string; y: number }[] }) => {
         width={300}
         height={300}
         innerRadius={75}
+        style={{ labels: { fontFamily: "sans-serif" } }}
         //colorScale={["red", "yellow", "green"]}
         data={props.data}
       />
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -135,221 +135,222 @@ const Stats = (props: PropTypesWithRefresh) => {
     ).flatMap((x) => x.dateCalorieTotals);
   }
 
-  console.log("assoc-res: ", assocResults);
   return (
     <Layout gqlClient={props.gqlClient} setToken={props.setToken}>
-      <h1 className="text-2xl pl-8">Stats</h1>
-      {/* <label htmlFor="from-date">From: </label>
-      <input
-        id="from-date"
-        type="date"
-        value={from}
-        onChange={(e) => setFrom(e.target.value)}
-      /> */}
-      <div className="mx-6 max-w-sm flex flex-col justify-items-center w-full">
-        <div className="mb-6">
-          <label
-            htmlFor="from-date"
-            className="block font-bold text-center mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            From
-          </label>
-          <input
-            type="date"
-            id="from-date"
-            className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="..."
-            required
-            value={from}
-            onChange={(e) => setFrom(e.target.value)}
-          />
-        </div>
-        {/* <label htmlFor="to-date">To: </label>
-      <input
-        id="to-date"
-        type="date"
-        value={to}
-        onChange={(e) => setTo(e.target.value)}
-      /> */}
-        <div className="mb-6">
-          <label
-            htmlFor="to-date"
-            className="block font-bold text-center mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            To
-          </label>
-          <input
-            type="date"
-            id="to-date"
-            className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="..."
-            required
-            value={to}
-            onChange={(e) => setTo(e.target.value)}
-          />
-        </div>
-      </div>
-      Weights: {weightHistory.data?.weightBetweenDates.length}
-      Weight Goals: {weightGoals.data?.weightGoalsBetweenDates.length}
-      Distance Goals: {distanceGoals.data?.distanceGoalsBetweenDates.length}
-      Steps Goals: {stepsGoals.data?.stepsGoalsBetweenDates.length}
-      Pop Goals: {popGoals.data?.popGoalsBetweenDates.length}
-      Exercises: {exercises.data?.exercisesBetweenDates.length}
-      Meals: {meals.data?.mealsBetweenDates.length}
-      Moods: {moods.data?.moodsBetweenDates.length}
-      Sleep Habits: {sleepHabits.data?.sleepHabitsBetweenDates.length}
-      <h3 className="text-2xl pl-8">Weight Change</h3>
-      {weightHistory.data && weightHistory.data.weightBetweenDates.length > 1 && (
-        <Chart
-          data={weightHistory.data.weightBetweenDates
-            .sort(
-              (a, b) =>
-                (new Date(a.date_of_weight) as any) -
-                (new Date(b.date_of_weight) as any)
-            )
-            .map((wh) => ({
-              x: dayjs(wh.date_of_weight).format("MMM - DD - YY"),
-              y: wh.weight_in_lbs,
-            }))}
-        />
-      )}
-      <h3 className="text-2xl pl-8">Calorie Intake</h3>
-      {exercises.data?.exercisesBetweenDates.length && (
-        <div className="flex justify-center">
-          <div className="w-4/5">
-            <Chart
-              // data={[
-              //   ...new Set(
-              //     exercises.data?.exercisesBetweenDates.map(
-              //       (x) => x.date_of_exercise
-              //     )
-              //   ),
-              // ]
-              //   .sort()
-              //   .map((x) => ({
-              //     x: dayjs(x).format("M/D"),
-              //     y: exercises.data?.exercisesBetweenDates
-              //       .filter((ex) => ex.date_of_exercise === x)
-              //       .reduce((a, b) => a + b.calories, 0),
-              //   }))}
-              data={assocResults
-                .sort(
-                  (a, b) =>
-                    (new Date(a.date) as any) - (new Date(b.date) as any)
-                )
-                .map((item) => ({
-                  x: dayjs(item.date).format("M/D"),
-                  y: item.total,
-                }))}
-            />
+      <div className="flex justify-center">
+        <div className="w-4/5">
+          <h1 className="text-2xl text-center mb-6 mt-4">Stats</h1>
+          <div className="mx-auto max-w-sm flex justify-between w-full">
+            <div className="mb-6">
+              <label
+                htmlFor="from-date"
+                className="block font-bold text-center mb-2 text-sm text-gray-900 dark:text-gray-300"
+              >
+                From
+              </label>
+              <input
+                type="date"
+                id="from-date"
+                className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="..."
+                required
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-6">
+              <label
+                htmlFor="to-date"
+                className="block font-bold text-center mb-2 text-sm  text-gray-900 dark:text-gray-300"
+              >
+                To
+              </label>
+              <input
+                type="date"
+                id="to-date"
+                className="bg-gray-50 text-center border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="..."
+                required
+                value={to}
+                onChange={(e) => setTo(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-      )}
-      <h3 className="text-2xl pl-8">Sleep Quality</h3>
-      <div className="flex justify-center">
-        <div className="w-4/5">
-          <Pie
-            data={[
-              {
-                title: "Good",
-                value: countSleepQuality(sleepHabits, "GOOD"),
-                color: "#E38627",
-              },
-              {
-                title: "Decent",
-                value: countSleepQuality(sleepHabits, "DECENT"),
-                color: "#C13C37",
-              },
-              {
-                title: "Poor",
-                value: countSleepQuality(sleepHabits, "POOR"),
-                color: "#6A2135",
-              },
-            ]}
-          />
-        </div>
-      </div>
-      <h3 className="text-2xl pl-8">Sleep Amount</h3>
-      <div className="flex justify-center">
-        <div className="w-4/5">
-          <VPie
-            data={[
-              { x: "full", y: countSleepAmount(sleepHabits, "FULL") },
-              { x: "few", y: countSleepAmount(sleepHabits, "FEW") },
-              { x: "none", y: countSleepAmount(sleepHabits, "NONE") },
-              { x: "extra", y: countSleepAmount(sleepHabits, "EXTRA") },
-            ]}
-          />
-        </div>
-      </div>
-      {/* <VictoryPie
-        theme={VictoryTheme.material}
-        colorScale={["green", "orange", "red", "cyan"]}
-        //innerRadius={({ datum }) => datum.y + 10}
-        padAngle={5}
-        innerRadius={80}
-        //padAngle={({ datum }) => datum.y}
-        data={[
-          { x: "full", y: countSleepAmount(sleepHabits, "FULL") },
-          { x: "few", y: countSleepAmount(sleepHabits, "FEW") },
-          { x: "none", y: countSleepAmount(sleepHabits, "NONE") },
-          { x: "extra", y: countSleepAmount(sleepHabits, "EXTRA") },
-        ]}
-      /> */}
-      <h3 className="text-2xl pl-8">Stress Levels</h3>
-      {/* <svg width={300} height={300}>
-        <circle cx={150} cy={150} r={50} fill="#c43a31" />
-        <VictoryPie
-          standalone={false}
-          width={300}
-          height={300}
-          innerRadius={75}
-          //colorScale={["red", "yellow", "green"]}
-          data={[
-            { x: "High", y: countStressLevel(moods, "HIGH") },
-            { x: "Mid", y: countStressLevel(moods, "MODERATE") },
-            { x: "Low", y: countStressLevel(moods, "LOW") },
-          ]}
-        />
-      </svg> */}
-      <div className="flex flex-col items-center">
-        <VPieFull
-          data={[
-            { x: "High", y: countStressLevel(moods, "HIGH") },
-            { x: "Mid", y: countStressLevel(moods, "MODERATE") },
-            { x: "Low", y: countStressLevel(moods, "LOW") },
-          ]}
-        />
-      </div>
-      <h3 className="text-2xl pl-8">Moods</h3>
-      {/* list top three */}
-      <div className="flex justify-center">
-        <div className="w-4/5">
-          <VictoryChart polar theme={VictoryTheme.material}>
-            <VictoryArea
+          <p className="text-center underline text-lg">Recorded Data Found</p>
+          <div className="flex flex-wrap justify-around mt-4 mb-12">
+            <div>
+              <p>
+                Weights: {weightHistory.data?.weightBetweenDates.length} entries
+              </p>
+              <p>
+                Weight Goals: {weightGoals.data?.weightGoalsBetweenDates.length}{" "}
+                entries
+              </p>
+              <p>
+                Miles Goals:{" "}
+                {distanceGoals.data?.distanceGoalsBetweenDates.length} entries
+              </p>
+              <p>
+                Steps Goals: {stepsGoals.data?.stepsGoalsBetweenDates.length}{" "}
+                entries
+              </p>
+            </div>
+            <div>
+              <p>
+                Exercises: {exercises.data?.exercisesBetweenDates.length}{" "}
+                entries
+              </p>
+              <p>Meals: {meals.data?.mealsBetweenDates.length} entries</p>
+              <p>Moods: {moods.data?.moodsBetweenDates.length} entries</p>
+              <p>
+                Sleep Habits: {sleepHabits.data?.sleepHabitsBetweenDates.length}{" "}
+                entries
+              </p>
+            </div>
+          </div>
+
+          <h3 className="text-2xl text-center">Weight Change</h3>
+          <p className="text-center">
+            shows how your recorded weight has changed over time
+          </p>
+          {weightHistory.data &&
+            weightHistory.data.weightBetweenDates.length > 1 && (
+              <div className="flex justify-center mb-20">
+                <div className="w-4/5">
+                  <Chart
+                    data={weightHistory.data.weightBetweenDates
+                      .sort(
+                        (a, b) =>
+                          (new Date(a.date_of_weight) as any) -
+                          (new Date(b.date_of_weight) as any)
+                      )
+                      .map((wh) => ({
+                        x: dayjs(wh.date_of_weight).format("MMM - DD - YY"),
+                        y: wh.weight_in_lbs,
+                      }))}
+                  />
+                </div>
+              </div>
+            )}
+          <h3 className="text-2xl text-center">Calorie Intake</h3>
+          <p className="text-center">
+            shows your daily net calorie gain / loss across over time
+          </p>
+          {assocResults.length && (
+            <div className="flex justify-center mb-20">
+              <div className="w-4/5">
+                <Chart
+                  // data={[
+                  //   ...new Set(
+                  //     exercises.data?.exercisesBetweenDates.map(
+                  //       (x) => x.date_of_exercise
+                  //     )
+                  //   ),
+                  // ]
+                  //   .sort()
+                  //   .map((x) => ({
+                  //     x: dayjs(x).format("M/D"),
+                  //     y: exercises.data?.exercisesBetweenDates
+                  //       .filter((ex) => ex.date_of_exercise === x)
+                  //       .reduce((a, b) => a + b.calories, 0),
+                  //   }))}
+                  data={assocResults
+                    .sort(
+                      (a, b) =>
+                        (new Date(a.date) as any) - (new Date(b.date) as any)
+                    )
+                    .map((item) => ({
+                      x: dayjs(item.date).format("M/D"),
+                      y: item.total,
+                    }))}
+                />
+              </div>
+            </div>
+          )}
+          <h3 className="text-2xl text-center">Sleep Quality</h3>
+          <p className="text-center">
+            a breakdown of the recorded quality of your sleep
+          </p>
+          <div className="flex justify-center mt-8 mb-20">
+            <div className="w-4/5">
+              <Pie
+                data={[
+                  {
+                    title: "Good",
+                    value: countSleepQuality(sleepHabits, "GOOD"),
+                    color: "#E38627",
+                  },
+                  {
+                    title: "Decent",
+                    value: countSleepQuality(sleepHabits, "DECENT"),
+                    color: "#C13C37",
+                  },
+                  {
+                    title: "Poor",
+                    value: countSleepQuality(sleepHabits, "POOR"),
+                    color: "#6A2135",
+                  },
+                ]}
+              />
+            </div>
+          </div>
+          <h3 className="text-2xl text-center">Sleep Amount</h3>
+          <p className="text-center">
+            a breakdown of the recorded quantity of your sleep
+          </p>
+          <div className="flex justify-center mb-20">
+            <div className="w-4/5">
+              <VPie
+                data={[
+                  { x: "full", y: countSleepAmount(sleepHabits, "FULL") },
+                  { x: "few", y: countSleepAmount(sleepHabits, "FEW") },
+                  { x: "none", y: countSleepAmount(sleepHabits, "NONE") },
+                  { x: "extra", y: countSleepAmount(sleepHabits, "EXTRA") },
+                ]}
+              />
+            </div>
+          </div>
+          <h3 className="text-2xl text-center">Stress Levels</h3>
+          <p className="text-center">
+            an average of your recorded stress levels over time
+          </p>
+          <p className="text-center"> - hang in there!</p>
+          <div className="flex flex-col items-center mb-20">
+            <VPieFull
               data={[
-                { x: "RELAX", y: countMoodType(moods, "RELAXED") },
-                { x: "MOTIVATED", y: countMoodType(moods, "MOTIVATED") },
-                { x: "HAPPY", y: countMoodType(moods, "HAPPY") },
-                { x: "SATISFIED", y: countMoodType(moods, "SATISFIED") },
-                { x: "TIRED", y: countMoodType(moods, "TIRED") },
-                { x: "FRUSTRATED", y: countMoodType(moods, "FRUSTRATED") },
-                { x: "SAD", y: countMoodType(moods, "SAD") },
-                { x: "ANXIOUS", y: countMoodType(moods, "ANXIOUS") },
+                { x: "High", y: countStressLevel(moods, "HIGH") },
+                { x: "Mid", y: countStressLevel(moods, "MODERATE") },
+                { x: "Low", y: countStressLevel(moods, "LOW") },
               ]}
             />
-            <VictoryPolarAxis />
-          </VictoryChart>
+          </div>
+          <h3 className="text-2xl text-center">Well Being</h3>
+          <p className="text-center">
+            shows a ratio of your different moods over time
+          </p>
+          {/* list top three */}
+          <div className="flex justify-center mt-12 mb-8">
+            <div className="w-4/5">
+              <VictoryChart polar theme={VictoryTheme.material}>
+                <VictoryArea
+                  data={[
+                    { x: "RELAX", y: countMoodType(moods, "RELAXED") },
+                    { x: "MOTIVATED", y: countMoodType(moods, "MOTIVATED") },
+                    { x: "HAPPY", y: countMoodType(moods, "HAPPY") },
+                    { x: "SATISFIED", y: countMoodType(moods, "SATISFIED") },
+                    { x: "TIRED", y: countMoodType(moods, "TIRED") },
+                    { x: "FRUSTRATED", y: countMoodType(moods, "FRUSTRATED") },
+                    { x: "SAD", y: countMoodType(moods, "SAD") },
+                    { x: "ANXIOUS", y: countMoodType(moods, "ANXIOUS") },
+                  ]}
+                />
+                <VictoryPolarAxis />
+              </VictoryChart>
+            </div>
+          </div>
         </div>
       </div>
-      {/* <Pie
-        data={[
-          { title: "High", value: 10, color: "#E38627" },
-          { title: "Moderate", value: 15, color: "#C13C37" },
-          { title: "Low", value: 20, color: "#6A2135" },
-        ]}
-      />
-      <h3>Mood</h3> */}
     </Layout>
   );
 };
